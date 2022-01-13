3f0c0315bbed0de4f63f3f300e687ea75f4dfe4f
HelloPoly <96932020+HelloPoly@users.noreply.github.com>
2022-01-13 10:42:01 +0700

    Update info.php

--- manager/info.php
+++ manager/info.php
@@ -23,7 +23,7 @@ $result = get_user(1);
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous" />
-    <link rel="stylesheet" href="../src/css/style.css">
+    <link rel="stylesheet" href="../style.css">
 </head>
 
 <body>
